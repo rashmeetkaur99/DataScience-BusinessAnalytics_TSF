ba7ba0f68e86a96da32ef11f1f5ea7333a5684bd
rashmeetkaur99 <82873195+rashmeetkaur99@users.noreply.github.com>
2021-05-07 21:27:06 +0530

    Add files via upload

---
+++ task1_tsf.R
@@ -0,0 +1,47 @@
+#TASK-1
+
+# Prediction using supervised learning
+# Simple Linear Regression
+# Importing the dataset
+library(readxl)
+dataset = read_excel("C:\\Users\\lenovo\\Desktop\\internship-task\\regression.xlsx")
+View(dataset)
+nrow(dataset)
+dataset=as.data.frame(dataset)
+class(dataset)
+#loading library
+
+library(rpart)
+library(dplyr)
+library(caTools)
+
+#creating train and test sets
+plot(dataset)
+split_values<-sample.split(dataset$Scores,SplitRatio =0.9)
+
+train_sample<-subset(dataset,split_values==T)
+nrow(train_sample)
+test_sample<-subset(dataset,split_values==F)
+nrow(test_sample)
+# creating the apppropriate linear model
+plot(dataset)
+y=dataset$Scores
+x=dataset$Hours
+model=lm(y~x,train_sample)
+print(summary(model))
+
+result=predict(object = model ,newdata =  test_sample)
+result
+final=cbind(Scores=dataset$Scores,result)
+class(final)
+final=as.data.frame(final)
+final
+
+z=data.frame(pred=final$result,actual=dataset$Scores)
+z
+mse=sqrt(mean((z$actual-z$pred)^2))
+mse
+
+a=data.frame(x=9.25)
+final_result=predict(model, a)
+final_result
